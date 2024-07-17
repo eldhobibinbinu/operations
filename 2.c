e38fa9ef2d2131a5bc7d78cd65916df521ce6d1f
Eldho Bibin Binu <eldhobibinb@gmail.com>
2024-07-17 15:04:38 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int x,y,z;
+float k;
+int main()
+{
+printf("enter the value of x: ");
+scanf("%d",&x);
+printf("enter the value of y: ");
+scanf("%d",&y);
+printf("enter the value of z: ");
+scanf("%d",&z);
+k=x%y+z*z;
+printf("%f",k);
+
+}
+
+
---
+++ 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int x,y,z;
+float k;
+int main()
+{
+printf("enter the value of x: ");
+scanf("%d",&x);
+printf("enter the value of y: ");
+scanf("%d",&y);
+printf("enter the value of z: ");
+scanf("%d",&z);
+y=x+y-x;
+printf("%f",y);
+
+}
